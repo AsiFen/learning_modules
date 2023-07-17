05e1a1f3c4efbb23f6976fe0cf7e58dadbe5f8d7
Asisipho <missfente@gmail.com>
2023-07-17 15:39:17 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,3 @@
+export default function greet(name){
+    return "Hello, "+ name+ " - from ES6 modules"
+}
---
+++ greet_asi.js
@@ -0,0 +1,21 @@
+import greet from './greet.js'
+
+import chalk from 'chalk';
+
+import figlet from 'figlet';
+
+
+figlet('Asi', function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
+
+console.log(chalk.bgCyan.rgb(123, 45, 67).underline('Hello world!'));
+
+
+
+console.log(greet('Asi'));
